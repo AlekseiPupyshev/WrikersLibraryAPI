bdb2fb075da0c51e2431859d4b9d04e39b614907
cmashinho <cmashinho@gmail.com>
2015-09-06 14:19:56 +0300

    Added API

---
+++ WrikersLibraryAPI/APIManager.swift
@@ -0,0 +1,198 @@
+//
+//  APIManager.swift
+//  WrikersLibraryAPI
+//
+//  Created by cmashinho on 06/09/15.
+//  Copyright (c) 2015 cmashinho. All rights reserved.
+//
+
+import UIKit
+
+class APIManager: NSObject {
+    
+    // MARK: - Contacts
+    
+    class func getAllContacts() {
+        
+    }
+    
+    class func getContactsInAccount() {
+        
+    }
+    
+    class func getContactsByIDs() {
+        
+    }
+    
+    class func modifyContact() {
+        
+    }
+    
+    // MARK: - Accounts
+    
+    class func getAllAccounts() {
+        
+    }
+    
+    class func getAccountsByIDs() {
+        
+    }
+    
+    class func modifyAccount() {
+        
+    }
+    
+    // MARK: - Workflows
+    
+    class func getWorkflowsInAccount() {
+        
+    }
+    
+    class func createWorkfwlow() {
+        
+    }
+    
+    class func modifyWorkflow() {
+    
+    }
+    
+    // MARK: - Folders
+    
+    class func getFolderTreeInAllAccounts() {
+        
+    }
+    
+    class func getFolderTreeInAccount() {
+        
+    }
+    
+    class func getFolderTreeInFolders() {
+        
+    }
+    
+    class func getFoldersByIDs() {
+        
+    }
+    
+    class func createFolder() {
+        
+    }
+    
+    class func modifyFolder() {
+        
+    }
+    
+    class func deleteFoldersByIDs() {
+        
+    }
+    
+    // MARK: - Tasks
+    
+    class func getTasksInAllAccounts() {
+        
+    }
+    
+    class func getTasksInAccount() {
+        
+    }
+    
+    class func getTasksInFolder() {
+        
+    }
+    
+    class func getTasksByIDs() {
+        
+    }
+    
+    class func createTask() {
+        
+    }
+    
+    class func modifyTask() {
+        
+    }
+    
+    class func deleteTaskByIDs() {
+        
+    }
+    
+    // MARK: - Comments
+    
+    class func getCommentsInAllAccounts() {
+        
+    }
+    
+    class func getCommentsInAccount() {
+        
+    }
+    
+    class func getCommentsInFolder() {
+        
+    }
+    
+    class func getCommentsInTask() {
+    
+    }
+    
+    class func getCommentsByIDs() {
+        
+    }
+    
+    class func createCommentInFolder() {
+        
+    }
+    
+    class func createCommentInComment() {
+        
+    }
+    
+    class func updateCommentInFolder() {
+        
+    }
+    
+    class func deleteCommentByID() {
+        
+    }
+    
+    // MARK: - Attachemnts
+    
+    class func getAttachmentsInFolder() {
+        
+    }
+    
+    class func getAttachmentsInTask() {
+        
+    }
+    
+    class func getAttachmentsByIDs() {
+        
+    }
+    
+    class func downloadWrikeAttachmentByID() {
+        
+    }
+    
+    class func getAccessURLForAttachmentByID() {
+        
+    }
+    
+    class func downloadAttachmentPreviewByID() {
+        
+    }
+    
+    class func createWrikeAttachmentInFolder() {
+        
+    }
+    
+    class func createWrikeAttachmentInTaks() {
+        
+    }
+    
+    class func updateAttachmentByID() {
+        
+    }
+    
+    class func deleteAttachmentByID() {
+        
+    }
+}
---
+++ WrikersLibraryAPI/APIMethodsManager.swift
@@ -0,0 +1,15 @@
+//
+//  APIMethodsManager.swift
+//  WrikersLibraryAPI
+//
+//  Created by cmashinho on 06/09/15.
+//  Copyright (c) 2015 cmashinho. All rights reserved.
+//
+
+import UIKit
+
+class APIMethodsManager: NSObject {
+    var sharedInstance = APIMethodsManager()
+    
+    
+}
